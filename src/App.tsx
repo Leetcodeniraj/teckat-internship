1f718c9cbd8a42755e54eaa75f28269a9536231f
leetcodeniraj <nirajthakurrudra@199gmail.comgit config --global user.name leetcodeniraj>
2024-06-11 17:32:41 +0530

    use of useEffect and Dependency

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,42 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App(){
-const [data , setData] = useState<string>();
+// predefined fuctions are called hook. Example useEffect
+
+
+const [data , setData] = useState<number>(0);
+const [numberData , setNumberdata] = useState<number>(0);
 
 useEffect( () => {
-  setData("world");
-},[]);
+  addData();
+},[numberData]);
 
-// predefined fuctions are called hook like useEffect
+const addData = () => {
+  const oldData = data;
+  const newData = oldData+1;
+  setData(newData);
+  console.log(newData);
+};
+
+const decrementData = () => {
+  setNumberdata(numberData -1);
+};
+
+
 
   return (
     <>
-      <div>{data}</div>
+
+    <button title="title" onClick ={addData} className={`number ${data}`}>
+       add Data </button>
+   
+   
+    <div>Incremented data:{data}</div>
+    <br/>
+
+    <button  onClick ={decrementData} className={`decrement ${numberData}`}> decrement </button>
+     
+      <div>decremented Data:{numberData} </div>
      
     </>
   )
