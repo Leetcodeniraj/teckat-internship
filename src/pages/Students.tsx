e336f4f757dcd0453aff1e7c930d803dee71665b
leetcodeniraj <nirajthakurrudra@199gmail.comgit config --global user.name leetcodeniraj>
2024-06-20 18:21:44 +0530

    store array in local storage

--- src/pages/Students.tsx
+++ src/pages/Students.tsx
@@ -1,13 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { IStudent } from '../interface/student';
+import "./Student.scss"
 
-const Students: React.FC= () => {
+const Students: React.FC = () => {
+const [allStudents, setAllStudents]= useState<IStudent[]>();
+``
+
+useEffect(() => {
+   const student = JSON.parse(localStorage.getItem("students") as string);
+  
+   setAllStudents(student)
+  console.log("student data", student);
+  
+
+  },[]);
+
+  
   return (
     <>
-      <button title="Create Student">
+    
+      <button title="Create Student" className="create_button">
         <Link to="/student/Create-Student">CreateStudent</Link>
       </button>
-      
+
+      <br/>
+      <hr/>
+
+
+      <table>
+        <thead>
+          <tr>
+            <td>SN.</td>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Phone Number</td>
+            <td>Email ID</td>
+          </tr>
+        </thead>
+        <tbody>
+          {allStudents?.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{item.name}</td>
+                <td>{item.age}</td>
+                <td>{item.phone}</td>
+                <td>{item.email}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </>
   )
 }
